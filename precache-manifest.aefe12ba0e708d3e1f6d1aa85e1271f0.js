104d106b92c46a2f8427fcba0c2fe02ae781aae8
jpap1021 <juan.arteaga8710@gmail.com>
2019-08-19 19:39:52 -0500

    Updates

---
+++ precache-manifest.aefe12ba0e708d3e1f6d1aa85e1271f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c4120e2ab4c7f54742f6480d575f02",
+    "url": "/mockup.tl/index.html"
+  },
+  {
+    "revision": "be732a6c2973443bc9b1",
+    "url": "/mockup.tl/static/css/main.a1e5f9c4.chunk.css"
+  },
+  {
+    "revision": "c008a769c8aa88fbc095",
+    "url": "/mockup.tl/static/js/2.f0187146.chunk.js"
+  },
+  {
+    "revision": "be732a6c2973443bc9b1",
+    "url": "/mockup.tl/static/js/main.74e237e6.chunk.js"
+  },
+  {
+    "revision": "82f5ad6bc00c2043ab12",
+    "url": "/mockup.tl/static/js/runtime~main.9c51c44a.js"
+  }
+]);
